5a466fb596e6de2b1cf84cdaa578a2f432ee1bf9
Alexandra <alexbushueva27@gmail.com>
2021-04-14 01:15:28 +0300

    Add styling buttons

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,15 +1,29 @@
 import React from 'react'
 import {StyleSheet, View, Text, Button} from "react-native";
+import {THEME} from '../theme'
 
 export const TodoScreen = (props) => {
     return (
         <View>
             <Text>{props.todo.title}</Text>
-            <Button title={'Back'} onPress={props.goBack}/>
+            <View style={styles.buttons}>
+                <View style={styles.button}>
+                    <Button title={'Back'} color={THEME.GREY_COLOR} onPress={props.goBack}/>
+                </View>
+                <View style={styles.button}>
+                    <Button title={'Delete'} color={THEME.DANGER_COLOR} onPress={() => console.log('To remove')}/>
+                </View>
+            </View>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    
+    buttons: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    button: {
+        width: '40%'
+    }
 })
