3353c5f79ad0bed01db08e114a1c0ac79e52233b
Alexandra <alexbushueva27@gmail.com>
2021-04-15 00:57:07 +0300

    Add Card component and styles

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import {StyleSheet, View, Text, Button} from "react-native";
 import {THEME} from '../theme'
+import {AppCard} from "../components/ui/AppCard";
 
 export const TodoScreen = (props) => {
     return (
         <View>
-            <Text>{props.todo.title}</Text>
+            <AppCard style={styles.card}>
+                <Text style={styles.title}>{props.todo.title}</Text>
+                <Button title={'Edit'}/>
+            </AppCard>
             <View style={styles.buttons}>
                 <View style={styles.button}>
                     <Button title={'Back'} color={THEME.GREY_COLOR} onPress={props.goBack}/>
@@ -23,7 +27,14 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         justifyContent: 'space-between'
     },
+    card: {
+        marginBottom: 20,
+        padding: 15
+    },
     button: {
         width: '40%'
+    },
+    title: {
+        fontSize: 20
     }
 })
