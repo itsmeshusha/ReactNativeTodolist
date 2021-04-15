3ac58bb4cd338bcce46e9cda010ef29b5465bc9e
Alexandra <alexbushueva27@gmail.com>
2021-04-16 00:43:36 +0300

    Add image on screen without tasks

--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,23 +1,45 @@
 import React from 'react'
-import {StyleSheet, View, FlatList} from "react-native";
+import {StyleSheet, View, FlatList, Image} from "react-native";
 import {AddTodo} from "../components/AddTodo";
 import {Todo} from "../components/Todo";
 
 export const MainScreen = (props) => {
+    let content = (
+        <FlatList
+            keyExtractor={item => item.id.toString()}
+            data={props.todos}
+            renderItem={({item}) => <Todo todo={item} key={item.id}
+                                          onRemove={props.removeTodo}
+                                          onOpen={props.openTodo}
+            />}
+        />
+
+    )
+
+    if (props.todos.length === 0) {
+        content = <View style={styles.imgWrap}>
+            <Image style={styles.image} source={require('../../assets/no-items.png')}/>
+        </View>
+    }
+
     return (
         <View>
             <AddTodo onSubmit={props.addTodo}/>
-
-            <FlatList
-                keyExtractor={item => item.id.toString()}
-                data={props.todos}
-                renderItem={({item}) => <Todo todo={item} key={item.id}
-                                              onRemove={props.removeTodo}
-                                              onOpen={props.openTodo}
-                />}
-            />
+            {content}
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    imgWrap: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 10,
+        height: 300
+    },
+    image: {
+        height: '100%',
+        width: '100%',
+        resizeMode: 'contain'
+    }
+})
