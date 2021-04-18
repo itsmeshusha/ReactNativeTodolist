83af7fba7587d657a17300225ff8eac94d8f37fc
Alexandra <alexbushueva27@gmail.com>
2021-04-18 23:58:09 +0300

    Add fonts

--- App.js
+++ App.js
@@ -1,23 +1,32 @@
 import React, {useState} from 'react';
 import {StyleSheet, Text, View, FlatList, Alert} from 'react-native';
 import * as Font from 'expo-font';
+import {AppLoading} from 'expo';
 import {Navbar} from './src/components/Navbar'
 import {MainScreen} from "./src/screens/MainScreen";
 import {TodoScreen} from "./src/screens/TodoScreen";
 
-async function loadApp () {
+async function loadApp() {
     await Font.loadAsync({
-        'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf')
+        'roboto-regular': require('./assets/fonts/Roboto-Regular.ttf'),
         'roboto-bold': require('./assets/fonts/Roboto-Bold.ttf')
     })
 }
 
 export default function App() {
+    const [isReady, setIsReady] = useState(false)
     const [todoId, setTodoId] = useState(null)
     const [todos, setTodos] = useState([
         {id: '1', title: 'Learn Javascript'},
         {id: '2', title: 'Learn React Native'}
     ])
+
+    if (!isReady) {
+        return <AppLoading startAsync={loadApp}
+                           onError={err => console.log(err)}
+                           onFinish={() => setIsReady(true)}
+        />
+    }
 
     const addTodo = (title) => {
         setTodos(prevTodos => [
