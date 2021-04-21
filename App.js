bc3f735d4a340015b87df3538af3eb07e496e63c
Alexandra <alexbushueva27@gmail.com>
2021-04-22 01:02:57 +0300

    Add adaptive styles and screen orientation

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {AppLoading} from 'expo';
 import {Navbar} from './src/components/Navbar'
 import {MainScreen} from "./src/screens/MainScreen";
 import {TodoScreen} from "./src/screens/TodoScreen";
+import {THEME} from "./src/theme";
 
 async function loadApp() {
     await Font.loadAsync({
@@ -97,7 +98,7 @@ export default function App() {
 
 const styles = StyleSheet.create({
     container: {
-        paddingHorizontal: 30,
+        paddingHorizontal: THEME.PADDING_HORIZONTAL,
         paddingVertical: 20
     },
 });
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,18 +1,34 @@
-import React from 'react'
-import {StyleSheet, View, FlatList, Image} from "react-native";
+import React, {useState, useEffect} from 'react'
+import {StyleSheet, View, FlatList, Image, Dimensions} from "react-native";
 import {AddTodo} from "../components/AddTodo";
 import {Todo} from "../components/Todo";
+import {THEME} from "../theme";
 
 export const MainScreen = (props) => {
+    const [deviceWidth, setDeviceWidth] = useState(Dimensions.get('window').width - THEME.PADDING_HORIZONTAL * 2)
+
+    useEffect(() => {
+        const update = () => {
+            const width = Dimensions.get('window').width - THEME.PADDING_HORIZONTAL * 2
+            setDeviceWidth(width)
+        }
+        Dimensions.addEventListener('change', update)
+        return () => {
+            Dimensions.removeEventListener('change', update)
+        }
+    })
+
     let content = (
-        <FlatList
-            keyExtractor={item => item.id.toString()}
-            data={props.todos}
-            renderItem={({item}) => <Todo todo={item} key={item.id}
-                                          onRemove={props.removeTodo}
-                                          onOpen={props.openTodo}
-            />}
-        />
+        <View style={{width: deviceWidth}}>
+            <FlatList
+                keyExtractor={item => item.id.toString()}
+                data={props.todos}
+                renderItem={({item}) => <Todo todo={item} key={item.id}
+                                              onRemove={props.removeTodo}
+                                              onOpen={props.openTodo}
+                />}
+            />
+        </View>
 
     )
 
