928f32ea81810ce5936363f1a21c4878ae2bc2b8
Alexandra <alexbushueva27@gmail.com>
2021-04-23 00:45:02 +0300

    Create todoReducer

--- src/MainLayout.js
+++ src/MainLayout.js
@@ -7,55 +7,54 @@ import {TodoScreen} from "./screens/TodoScreen";
 import {TodoContext} from "./context/todo/todoContext";
 
 export const MainLayout = () => {
-    const todosContext = useContext(TodoContext)
+    const {todos, addTodo, removeTodo, updateTodo} = useContext(TodoContext)
     const [todoId, setTodoId] = useState(null)
-    const [todos, setTodos] = useState([])
 
-    const addTodo = (title) => {
-        setTodos(prevTodos => [
-            ...prevTodos,
-            {
-                id: Date.now().toString(),
-                title
-            }
-        ])
-    }
-
-    const removeTodo = (id) => {
-        const todo = todos.find(t => t.id === id)
-        Alert.alert(
-            'Delete task',
-            `Are you sure to delete "${todo.title}" task?`,
-            [
-                {
-                    text: 'Cancel',
-                    style: 'cancel'
-                },
-                {
-                    text: 'Delete',
-                    style: 'destructive',
-                    onPress: () => {
-                        setTodoId(null)
-                        setTodos(prev => prev.filter(todo => todo.id !== id))
-                    }
-                }
-            ],
-            {cancelable: false}
-        )
-
-    }
-
-    const updateTodo = (id, title) => {
-        setTodos(state => state.map(todo => {
-            if (todo.id === id) {
-                todo.title = title
-            }
-            return todo
-        }))
-    }
+    // const addTodo = (title) => {
+    //     setTodos(prevTodos => [
+    //         ...prevTodos,
+    //         {
+    //             id: Date.now().toString(),
+    //             title
+    //         }
+    //     ])
+    // }
+    //
+    // const removeTodo = (id) => {
+    //     const todo = todos.find(t => t.id === id)
+    //     Alert.alert(
+    //         'Delete task',
+    //         `Are you sure to delete "${todo.title}" task?`,
+    //         [
+    //             {
+    //                 text: 'Cancel',
+    //                 style: 'cancel'
+    //             },
+    //             {
+    //                 text: 'Delete',
+    //                 style: 'destructive',
+    //                 onPress: () => {
+    //                     setTodoId(null)
+    //                     setTodos(prev => prev.filter(todo => todo.id !== id))
+    //                 }
+    //             }
+    //         ],
+    //         {cancelable: false}
+    //     )
+    //
+    // }
+    //
+    // const updateTodo = (id, title) => {
+    //     setTodos(state => state.map(todo => {
+    //         if (todo.id === id) {
+    //             todo.title = title
+    //         }
+    //         return todo
+    //     }))
+    // }
 
     let content = <MainScreen addTodo={addTodo}
-                              todos={todosContext.todos}
+                              todos={todos}
                               removeTodo={removeTodo}
                               openTodo={setTodoId}/>
 
